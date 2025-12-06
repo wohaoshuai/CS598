a7bdad409ce4e2bdb6a29452a6c07e39eeeada88
Tony Kim <wukelong123@gmail.com>
2025-12-06 17:21:33 +0800

    upload

--- src/scripts/pretrain/simclr.sh
+++ src/scripts/pretrain/simclr.sh
@@ -3,16 +3,16 @@
 
 
 python src/models/selfsupervised/trainers/vicreg_trainer.py \
-    --device $CUDA_VISIBLE_DEVICES \
+    --device 0 \
     --vision-backbone resnet34 \
-    --job_number ${SLURM_JOBID} \
-    --file_name VICREG-${SLURM_JOBID} \
+    --job_number 123456 \
+    --file_name 123456 \
     --epochs 300 \
     --transforms_cxr simclrv2 \
     --temperature 0.01 \
     --vicreg \
     --num_gpu 1 \
-    --batch_size 256 \
+    --batch_size 5 \
     --lr 0.5989 \
     --pretrain_type simclr \
     --mode train \
