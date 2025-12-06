987e52f99a4a3f874896df1ac3de0bd9ed731728
Tony Kim <wukelong123@gmail.com>
2025-12-06 17:46:12 +0800

    Update simclr_trainer.py

--- src/models/selfsupervised/trainers/simclr_trainer.py
+++ src/models/selfsupervised/trainers/simclr_trainer.py
@@ -99,9 +99,9 @@ if __name__ == '__main__':
     if args.mode == 'train':
         print('==> training')        
         train(model, args, train_dl, val_dl,
-            logger=neptune_logger,
+            logger=None,
             load_state_prefix=args.load_state_simclr)
-        test(model, args, test_dl, logger=neptune_logger)
+        test(model, args, test_dl, logger=None)
         
     elif args.mode == 'eval':
         if args.eval_set=='val':
@@ -112,7 +112,7 @@ if __name__ == '__main__':
             test_dl=train_dl
         else:
             print('==> evaluating on the test set')
-        test(model, args, test_dl, logger=neptune_logger)
+        test(model, args, test_dl, logger=None)
                 
     else:
         raise ValueError("Incorrect value for args.mode")
